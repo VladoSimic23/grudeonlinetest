733e88b5e55de13ab5737b31cdcc70e8cf71b278
Vlado Simic <vladosimic525@gmail.com>
2024-03-20 21:00:21 +0100

    prodcution ready 5

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,26 @@ const nextConfig = {
   images: {
     domains: ["193.108.130.23"],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/:slug",
-  //       destination: "/category/sport/:slug",
-  //     },
-  //     {
-  //       source: "/tag/:slug",
-  //       destination: "/category/tag/:slug",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      // {
+      //   source: "/category/sport/:slug",
+      //   destination: "/:slug",
+      // },
+      // {
+      //   source: "/tag/:slug",
+      //   destination: "/category/tag/:slug",
+      // },
+      // {
+      //   source: "/:slug",
+      //   destination: "/category/sport/:slug",
+      // },
+      // {
+      //   source: "/tag/:slug",
+      //   destination: "/category/tag/:slug",
+      // },
+    ];
+  },
 };
 
 export default nextConfig;
