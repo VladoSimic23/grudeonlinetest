7ba8b36022119010dca4c75440bf68f2885f44bc
Vlado Simic <vladosimic525@gmail.com>
2024-03-24 15:51:16 +0100

    load test

---
+++ src/app/category/page.tsx
@@ -0,0 +1,12 @@
+// pages/loaderio-verification.js
+
+import React from "react";
+
+const LoaderIOVerificationPage = () => (
+  <div>
+    <meta name="loaderio" content="loaderio-f72b2c84f69f92c6c71c64a7f2dbbb1d" />
+    <p>loaderio-f72b2c84f69f92c6c71c64a7f2dbbb1d</p>
+  </div>
+);
+
+export default LoaderIOVerificationPage;
