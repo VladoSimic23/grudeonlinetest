81d7eb9166e5de2c7dcd01af8ebd09b86ced8474
Vlado Simic <vladosimic525@gmail.com>
2024-03-25 19:01:02 +0100

    nav edit

--- src/app/components/Categories/Categories.tsx
+++ src/app/components/Categories/Categories.tsx
@@ -1,38 +1,94 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
+import styles from "../../css/mainCss/mainStyle.module.css";
 
 const Categoires = ({ toggle }: { toggle: any }) => {
+  const pathname = usePathname();
   return (
-    <ul>
+    <ul className={styles.navLinks}>
       <li onClick={toggle}>
-        <Link href={"/"}>Naslovnica</Link>
+        <Link className={`${pathname === "/" ? styles.active : ""}`} href={"/"}>
+          Naslovnica
+        </Link>
       </li>
       <li onClick={toggle}>
-        <Link href={"/category/vijesti/"}>Vijesti</Link>
+        <Link
+          className={`${pathname === "/category/vijesti" ? styles.active : ""}`}
+          href={"/category/vijesti"}
+        >
+          Vijesti
+        </Link>
       </li>
       <li onClick={toggle}>
-        <Link href={"/category/crna-kronika"}>Crna Kronika</Link>
+        <Link
+          className={`${
+            pathname === "/category/crna-kronika" ? styles.active : ""
+          }`}
+          href={"/category/crna-kronika"}
+        >
+          Crna Kronika
+        </Link>
       </li>
       <li onClick={toggle}>
-        <Link href={"/category/sport/"}>Sport</Link>
+        <Link
+          className={`${pathname === "/category/sport" ? styles.active : ""}`}
+          href={"/category/sport"}
+        >
+          Sport
+        </Link>
       </li>
       <li onClick={toggle}>
-        <Link href={"/category/politika"}>Politika</Link>
+        <Link
+          className={`${
+            pathname === "/category/politika" ? styles.active : ""
+          }`}
+          href={"/category/politika"}
+        >
+          Politika
+        </Link>
       </li>
       <li onClick={toggle}>
-        <Link href={"/category/gospodarstvo/"}>Gospodarstvo</Link>
+        <Link
+          className={`${
+            pathname === "/category/gospodarstvo" ? styles.active : ""
+          }`}
+          href={"/category/gospodarstvo"}
+        >
+          Gospodarstvo
+        </Link>
       </li>
       <li onClick={toggle}>
-        <Link href={"/category/kultura"}>Kultura</Link>
+        <Link
+          className={`${pathname === "/category/kultura" ? styles.active : ""}`}
+          href={"/category/kultura"}
+        >
+          Kultura
+        </Link>
       </li>
       <li onClick={toggle}>
-        <Link href={"/category/zanimljivosti"}>Zanimljivosti</Link>
+        <Link
+          className={`${
+            pathname === "/category/zanimljivosti" ? styles.active : ""
+          }`}
+          href={"/category/zanimljivosti"}
+        >
+          Zanimljivosti
+        </Link>
       </li>
       <li onClick={toggle}>
-        <Link href={"/category/lifestyle"}>Lifestyle</Link>
+        <Link
+          className={`${
+            pathname === "/category/lifestyle" ? styles.active : ""
+          }`}
+          href={"/category/lifestyle"}
+        >
+          Lifestyle
+        </Link>
       </li>
       <li onClick={toggle}>
-        <Link href={"#"}>Osmrtnice</Link>
+        <Link href={"https://www.osmrtnica.ba/"}>Osmrtnice</Link>
       </li>
     </ul>
   );
