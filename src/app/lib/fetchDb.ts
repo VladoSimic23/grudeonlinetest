b70ac26fa6b445501afe2354b3783c5195540019
Vlado Simic <vladosimic525@gmail.com>
2024-04-25 19:18:48 +0200

    increased revalidate time

--- src/app/lib/fetchDb.ts
+++ src/app/lib/fetchDb.ts
@@ -94,7 +94,7 @@ export async function fetchAPI(query: any, { variables }: any = {}) {
   };
 
   const res = await fetch(temporaryApiUrl, {
-    next: { revalidate: 1, tags: ["collection"] },
+    next: { revalidate: 12000, tags: ["collection"] },
     method: "POST",
     headers,
     body: JSON.stringify({
